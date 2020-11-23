2729cf2a26aa7f50de524fe1c63081b374fae7af
Kewen Macedo Silva <kewen.macedo@gmail.com>
2020-11-23 08:36:30 -0200

    create router posts

---
+++ backend/src/routes.js
@@ -0,0 +1,10 @@
+const routes = require('express').Router();
+const multer = require('multer');
+const multerConfig = require("./config/multer");
+
+routes.post("/posts", multer().single('file'),(req, res) => {
+  console.log(req.file);
+  return res.json({ hello: "Ok" })
+});
+
+module.exports = routes;
