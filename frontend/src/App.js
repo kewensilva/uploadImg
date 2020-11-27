6555e585ac28a1ebd0678e0ab191af975358d0ce
Kewen Macedo Silva <kewen.macedo@gmail.com>
2020-11-27 09:19:07 -0200

    styles finished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
 import GlobalStyle from "./styles/global";
 import { Container, Content } from "./pages/styles"
-import { render } from "@testing-library/react";
 
+import Upload from "./componnents/uploads";
 
 function App() {
 
   return (
     <Container>
-      <Content> Teste </Content>
+      <Content>
+        <Upload />
+      </Content>
       <GlobalStyle />
     </Container>
 
--- frontend/src/componnents/uploads/index.js
+++ frontend/src/componnents/uploads/index.js
@@ -5,6 +5,16 @@ import Dropzone from "react-dropzone";
 import { DropContainer, UploadMessage } from "./styles";
 
 export default class Upload extends Component {
+renderDragMessage = (isDragActive, isDragReject) => {
+  if(! isDragActive){
+    return <UploadMessage> Arraste arquivos aqui</UploadMessage>
+  }
+  if(isDragReject) {
+    return <UploadMessage type="error"> Arquivo não suportado</UploadMessage>
+  }
+  return <UploadMessage type="sucess">Solte os arquivos aqui</UploadMessage>
+}
+
   render() {
     return (
       <Dropzone accept="image/*" onDropAccepted={() => {}}>
@@ -13,6 +23,7 @@ export default class Upload extends Component {
         isDragActive={isDragActive}
         isDragReject={isDragReject}>
           <input {...getInputProps()} />
+          {this.renderDragMessage(isDragActive, isDragReject)}
         </DropContainer>
         ) } 
       </Dropzone>
